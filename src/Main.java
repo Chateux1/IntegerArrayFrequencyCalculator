97488ac0cd629fe62df3ba3dcbd8f9741f432684
Chateux1 <m.satas@gmail.com>
2018-09-14 00:17:09 +0300

    Major code refactor. No additional functionallity, only refactor code to smaller methods. Create get and set methods to all necessary variables. Change input test sample data in 'main'.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World!");
-    	int[] a = {1,2,3,3,3,7,7,8};
-        new NumberArray(a);
+		int[] array = {1,2,2,3,4,4,5,6,6,7,8,8,9};
+		NumberArray test = new NumberArray(array);
+		System.out.println(Arrays.toString(test.getValues()));
+		System.out.println(Arrays.toString(test.getFrequencies()));
 	}
 }
--- src/NumberArray.java
+++ src/NumberArray.java
@@ -1,58 +1,76 @@
 import java.util.Arrays;
-//import java.awt;
-//import javax.swing;
 
 public class NumberArray {
-		
-		int value;
-	    int frequency;
-
-	    public int getValue () {
-	        return this.value;
-	    }
-
-	    public int getFrequency () {
-	        return this.frequency;
-	    }
-
-	    public void setup (int value) {
-	        this.value = value;
-	        this.frequency = 0;
-	    }
-
-	    public void setIncrease () {
-	        this.frequency++;
-	    }
-	    public NumberArray() {}
-	    
-	    public NumberArray(int[] input) {
-	    	//input
-	        int maxIndex = Arrays.stream(input).max().getAsInt()+1;
-
-	        //Initialise array of input values with 0 frequencies
-	        NumberArray[] numberArray = new NumberArray[maxIndex];
-	        for (int i = 0; i < maxIndex; i++) {
-	            numberArray[i] = new NumberArray();
-	            numberArray[i].setup(i);
-	        }
-
-	        //loop through input array and get frequencies
-	        for (int i = 0; i < input.length; i++) {
-	            int indexFromValue = input[i];
-	            numberArray[indexFromValue].setIncrease();
-	        }
-
-	        //create separate arrays for values and frequencies
-	        int[] values = new int[maxIndex]; 
-	        int[] frequencies = new int[maxIndex];
-	        for (int i = 0; i < maxIndex; i++) {
-	            values[i] = numberArray[i].getValue();
-	            frequencies[i] = numberArray[i].getFrequency();
-	        }
-
-	        //print out results
-	        System.out.println(Arrays.toString(input));
-	        System.out.println(Arrays.toString(values));
-	        System.out.println(Arrays.toString(frequencies));
-	    }
+	
+	private int[] values, frequencies, originalArray;
+	private int numberOfValues;
+	
+	private void setEmptyInputArray() {
+		for (int i = 0; i < getNumberOfValues(); i++) {
+			setValue(i, i);
+			setFrequency(i, 0);
+		}
+	}
+	
+	private void setOriginalArray(int[] input) {
+		numberOfValues = Arrays.stream(input).max().getAsInt()+1;
+		for (int i = 0; i < input.length; i++) {
+			originalArray[i] = input[i];
+		}
+	}
+	
+	private int getOriginalArrayValue(int i) {
+		return originalArray[i];
+	}
+	
+	private int getOriginalArrayLength() {
+		return originalArray.length;
+	}
+	
+	public int getNumberOfValues() {
+		return numberOfValues;
+	}
+	
+	private void setValue(int valueIndex, int valueToSet) {
+		values[valueIndex] = valueToSet;
+	}
+	
+	public int getValue(int i) {
+		return values[i];
+	}
+	
+	public int[] getValues() {
+		return values;
+	}
+	
+	private void setFrequency(int valueIndex, int valueToSet) {
+		frequencies[valueIndex] = valueToSet;
+	}
+	private void setIncreaseFrequency(int valueIndex) {
+		frequencies[valueIndex]++;
+	}
+	
+	public int getFrequency(int i) {
+		return frequencies[i];
+	}
+	
+	public int[] getFrequencies() {
+		return frequencies;
+	}
+	
+	private void setFrequencies() {
+        for (int i = 0; i < getOriginalArrayLength(); i++) {
+            int indexFromValue = getOriginalArrayValue(i);
+            setIncreaseFrequency(indexFromValue);
+        }
+	}
+	
+	public NumberArray(int[] numberArray) {
+		originalArray = new int[numberArray.length];
+		setOriginalArray(numberArray);
+		values = new int[getNumberOfValues()];
+		frequencies = new int[getNumberOfValues()];
+		setEmptyInputArray();
+		setFrequencies();
+	}	
 }
