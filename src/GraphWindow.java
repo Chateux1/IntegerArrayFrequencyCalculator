5f275b3fbdb8da315643d37fd73a5db2dc8504f3
Chateux1 <m.satas@gmail.com>
2018-09-14 01:30:49 +0300

    Use JList functionality example to display an array on JFrame.

--- src/GraphWindow.java
+++ src/GraphWindow.java
@@ -1,6 +1,20 @@
-public class GraphWindow {
+import javax.swing.*;
+
+public class GraphWindow extends JFrame {
 	
 	public GraphWindow() {
 		
+		DefaultListModel dlm = new DefaultListModel();
+		JList list = new JList(dlm);
+		JScrollPane scrollPane = new JScrollPane(list);
+		
+		int[] test = {1,2,5,4,3,1};
+		for (int value : test) {
+			dlm.addElement(value);
+		}
+		add(scrollPane);
+		pack();
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
 	}
 }
