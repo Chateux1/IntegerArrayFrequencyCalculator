b21e32936ff78764b9fac18ab4c8cc765113697c
Chateux1 <m.satas@gmail.com>
2018-09-13 23:30:13 +0300

    Create 'MainWindow' class to store the dialog box and all the element that will be displayed in the box. Create a test window in 'main'.

---
+++ src/MainWindow.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class MainWindow extends JFrame {
+	
+	public MainWindow() {
+		
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setSize(400,300);
+		setVisible(true);
+	}
+	
+}
