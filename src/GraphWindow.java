20e5594997a9245ff3eefd41a75ef14b95a3327c
Chateux1 <m.satas@gmail.com>
2018-09-16 00:19:25 +0300

    Stable version without formating.
    Add method to set table headers and frequencies as data.
    Remove unused methods and variables.
    Remove commented out fragments of code that wont be needed anymore.

--- src/GraphWindow.java
+++ src/GraphWindow.java
@@ -1,4 +1,3 @@
-//import java.awt.*;
 import java.util.Arrays;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
@@ -8,93 +7,47 @@ public class GraphWindow {
 	private JFrame frame;
 	private JTable table;
 	private JPanel panel;
-	private int[] labels;
-	private String[][] tableData;
-	private int numberOfColumns, numberOfRows;
-	
-	private void setNumberOfColumns(int number) {
-		
-		numberOfColumns = number;
-	}
-	
-	private void setNumberOfRows(int number) {
-		
-		numberOfRows = number;
-	}
-	
-	private String[][] setEmptyTable(int rows, int columns) {
-		
-		String[][] emptyTable = new String[rows][columns]; 
-		for (int i = 0; i < rows; i++) {
-			
-			for (int j = 0; j < columns; j++) {
-				
-				emptyTable[i][j] = " ";
-			}
-		}
-		return emptyTable;
-	}
-	
 	
 	private void setTableData(int maxFrequency, int numberOfValues, int[] frequencies) {
 		
-		int reusableFrequency;
 		for (int i = 0; i < numberOfValues; i++) {
+			
+			table.getModel().setValueAt(i, maxFrequency, i);
 				
-			reusableFrequency = 0;
-			if (frequencies[i] > reusableFrequency) {
+			int frequency = frequencies[i];
+			for (int j = maxFrequency; j > 0; j--) {
 				
-				for (int j = 0; j < frequencies[i]; j++) {
-
-					table.getModel().setValueAt("*", j, i);
+				if (frequency > 0) {
+					table.getModel().setValueAt("*", j-1, i);
+					frequency--;
 				}
 			}
 		}
 	}
-	
 	
 	public GraphWindow() {
 				
 		int[] array = {1,2,3,5,5,8,9};
 		int[] values = {1,2,3,4,5,6,7,8,9};
 		int[] frequencies = {1,1,1,0,2,0,0,1,1};
-		String[] valueStrings = new String[values.length];
-		
-		for (int i = 0; i < values.length; i++) {
-			valueStrings[i] = String.valueOf(values[i]);
-		}
-		Object[] valueObject = valueStrings;
 		
 		int maxFreq = Arrays.stream(frequencies).max().getAsInt();
 		int columns = values.length;
 		
-//		tableData = new String[maxFreq][columns];
-//		tableData = setEmptyTable(columns, maxFreq);
-//		setTableData(maxFreq, columns, frequencies);
-//		System.out.println(Arrays.deepToString(tableData));
-//		
-//		Object[][] tableDataObject = tableData;
-//		System.out.println(Arrays.deepToString(tableDataObject));
-		
 		frame = new JFrame();
 		panel = new JPanel();
-		//panel.setLayout(new BorderLayout());
-		DefaultTableModel model = new DefaultTableModel(maxFreq, valueStrings.length) ;
-		model.setColumnIdentifiers(valueStrings);
+		
+		DefaultTableModel model = new DefaultTableModel(maxFreq+1, values.length) ;
 		table = new JTable(model);
+		table.setTableHeader(null);
 		setTableData(maxFreq, columns, frequencies);
 		
-		//table = new JTable(tableDataObject, valueObject);
-		//table.setPreferredScrollableViewportSize(new Dimension(500,500));
-		//table.setFillsViewportHeight(true);
+		//JScrollPane tableContainer = new JScrollPane(table);
+		//panel.add(tableContainer);
+		panel.add(table);
 		
-		JScrollPane tableContainer = new JScrollPane(table);
-		
-		panel.add(tableContainer);
-		//panel.add(tableContainer, BorderLayout.CENTER);
 		frame.getContentPane().add(panel);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//frame.setSize(600,600);
 		frame.pack();
 		frame.setTitle("Graph Window");
 		frame.setVisible(true);
