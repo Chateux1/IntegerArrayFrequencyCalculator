a9f81f7fea94356b24ef5dc824488cf986a67efe
Chateux1 <m.satas@gmail.com>
2018-09-16 01:20:37 +0300

    Small changes
    Center allign cell contents
    Fix index bug
    Set BorderLayout for panel

--- src/GraphWindow.java
+++ src/GraphWindow.java
@@ -1,5 +1,7 @@
+import java.awt.BorderLayout;
 import java.util.Arrays;
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 
 public class GraphWindow {
@@ -12,7 +14,7 @@ public class GraphWindow {
 		
 		for (int i = 0; i < numberOfValues; i++) {
 			
-			table.getModel().setValueAt(i, maxFrequency, i);
+			table.getModel().setValueAt(i+1, maxFrequency, i);
 				
 			int frequency = frequencies[i];
 			for (int j = maxFrequency; j > 0; j--) {
@@ -25,6 +27,17 @@ public class GraphWindow {
 		}
 	}
 	
+	private void resizeTableColumns(int width, int columnCount) {
+		
+		for (int i = 0; i < columnCount; i++) {
+			
+			table.getColumnModel().getColumn(i).setPreferredWidth(width);
+			DefaultTableCellRenderer rightRenderer = new DefaultTableCellRenderer();
+			rightRenderer.setHorizontalAlignment(SwingConstants.CENTER);
+			table.getColumnModel().getColumn(i).setCellRenderer(rightRenderer);
+		}
+	}
+	
 	public GraphWindow(int[] values, int[] frequencies) {
 						
 		int maxFreq = Arrays.stream(frequencies).max().getAsInt();
@@ -34,15 +47,18 @@ public class GraphWindow {
 		table = new JTable(model);
 		table.setTableHeader(null);
 		table.setShowGrid(false);
+		table.setAutoResizeMode( JTable.AUTO_RESIZE_OFF );
 		setTableData(maxFreq, columns, frequencies);
+		resizeTableColumns(20, values.length);
 		
-		panel = new JPanel();
+		panel = new JPanel(new BorderLayout());
 		panel.add(table);
 		
 		frame = new JFrame();
 		frame.getContentPane().add(panel);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
+		frame.setSize(400,200);
+		//frame.pack();
 		frame.setTitle("Graph Window");
 		frame.setVisible(true);
 	}
